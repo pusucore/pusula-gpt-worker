d23e5ef9ffaf2b55af8bb5ec35b0d4f256798d8b
pusumedia <brucewayne8724@gmail.com>
2026-05-09 17:57:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,42 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const jobs = {};
+
 app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
+app.post("/create-image", async (req, res) => {
+
+  const jobId = Date.now().toString();
+
+  jobs[jobId] = {
+    status: "done",
+    imageUrl: "https://picsum.photos/1024"
+  };
+
+  res.json({
+    status: "processing",
+    jobId
+  });
+
+});
+
 app.get("/result/:jobId", (req, res) => {
-  res.json({ status: "not_found" });
+
+  const job = jobs[req.params.jobId];
+
+  if (!job) {
+    return res.json({
+      status: "not_found"
+    });
+  }
+
+  res.json(job);
+
 });
 
 app.listen(3000, "0.0.0.0", () => {
